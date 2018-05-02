77c388565b76113a06d609a5eef7462b79b9ee41
Nitsan Tzur <nitsant@checkpoint.com>
2018-05-02 14:58:08 +0300

    first commit1

---
+++ js/data.js
@@ -0,0 +1,28 @@
+data = {
+ "subject": [
+  {"id": 1, "title": "taaroz li maher", "title-heb": "רהמ יל זוראת", "lines": [""]},
+  {"id": 2, "title": "shmor li ve eshmor lecha", "lines": []},
+  {"id": 3, "title": "paam hanoar haya noar", "lines": []},
+  {"id": 4, "title": "ma she talui mityabesh ve nofel", "lines": []},
+  {"id": 5, "title": "chayal tatzdia", "lines": []},
+  {"id": 6, "title": "acharey she mitahavim", "lines": []},
+  {"id": 7, "title": "savta lama ha pe shelach colkach gadol", "lines": []},
+  {"id": 8, "title": "titze bahutz", "lines": []},
+  {"id": 9, "title": "meyutar meyutar", "lines": []},
+  {"id": 10, "title": "havanta shel ha poza", "lines": []},
+  {"id": 11, "title": "haorech lo kovea", "lines": []},
+  {"id": 12, "title": "GPS", "lines": []},
+  {"id": 13, "title": "sheket she yihye po", "lines": []},
+  {"id": 14, "title": "shmor et hayadayim shelcha leatzmecha", "lines": []},
+  {"id": 15, "title": "tziporey layla", "lines": []},
+  {"id": 16, "title": "mehaa harifa", "lines": []},
+  {"id": 17, "title": "ani be helem ani hamuma", "lines": []},
+  {"id": 18, "title": "ego rtrip": []},
+  {"id": 19, "title": "festival kan", "lines": []},
+  {"id": 20, "title": "sa le shalom hamavtechot bifnim", "lines": []},
+  {"id": 18, "title": "nire otchem be od kama shanim": []},
+  {"id": 18, "title": "kvish 1", "lines": []},
+  {"id": 18, "title": "al tidhof et ha af shelcha", "lines": []},
+  {"id": 18, "title": "eize chom", "lines": []},
+  "1","2","3"]
+}
