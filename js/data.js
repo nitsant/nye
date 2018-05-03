f88f6d64bf40f42472260c28b4462c28be4736eb
Nitsan-home <nitsanc@gmail.com>
2018-05-04 00:18:52 +0300

    grid items

--- js/data.js
+++ js/data.js
@@ -1,39 +1,50 @@
 data = {
- "subject": [
-  {"id": 1, "title": "taaroz li maher", "title-heb": "תארוז לי מהר",
+ "items": [
+  {"id": "grid-item-1", "title": "taaroz li maher", "title-heb": "תארוז לי מהר",
     "lines":
     [
       "פיתה עם שווארמה"
      ,"לחמניה עם נקניקיה"
      ,"פיצה"
-     ,"חמניה עם נקניקיה"
-     ,"לחמניה עם נקניקיה"
-     ,"לחמניה עם נקניקיה"
-     ,"לחמניה עם נקניקיה"
-     ,"לחמניה עם נקניקיה"
+     ,"המבורגר"
+     ,"סושי"
+     ,"סודוך"
+     ,"בורקס"
    ]},
-  {"id": 2, "title": "shmor li ve eshmor lecha", "lines": []},
-  {"id": 3, "title": "paam hanoar haya noar", "lines": []},
-  {"id": 4, "title": "ma she talui mityabesh ve nofel", "lines": []},
-  {"id": 5, "title": "chayal tatzdia", "lines": []},
-  {"id": 6, "title": "acharey she mitahavim", "lines": []},
-  {"id": 7, "title": "savta lama ha pe shelach colkach gadol", "lines": []},
-  {"id": 8, "title": "titze bahutz", "lines": []},
-  {"id": 9, "title": "meyutar meyutar", "lines": []},
-  {"id": 10, "title": "havanta shel ha poza", "lines": []},
-  {"id": 11, "title": "haorech lo kovea", "lines": []},
-  {"id": 12, "title": "GPS", "lines": []},
-  {"id": 13, "title": "sheket she yihye po", "lines": []},
-  {"id": 14, "title": "shmor et hayadayim shelcha leatzmecha", "lines": []},
-  {"id": 15, "title": "tziporey layla", "lines": []},
-  {"id": 16, "title": "mehaa harifa", "lines": []},
-  {"id": 17, "title": "ani be helem ani hamuma", "lines": []},
-  {"id": 18, "title": "ego rtrip": []},
-  {"id": 19, "title": "festival kan", "lines": []},
-  {"id": 20, "title": "sa le shalom hamavtechot bifnim", "lines": []},
-  {"id": 18, "title": "nire otchem be od kama shanim": []},
-  {"id": 18, "title": "kvish 1", "lines": []},
-  {"id": 18, "title": "al tidhof et ha af shelcha", "lines": []},
-  {"id": 18, "title": "eize chom", "lines": []},
+  {"id": "grid-item-2", "title": "shmor li ve eshmor lecha", "title-heb": "שמור לי ואשמור לך",
+  "lines": []},
+  {"id": "grid-item-3", "title": "paam hanoar haya noar", "title-heb": "פעם הנוער היה נוער",
+  "lines": []},
+  {"id": "grid-item-4", "title": "ma she talui mityabesh ve nofel", "title-heb": "מה שתלוי מתייבש ונופל",
+  "lines": []},
+  {"id": "grid-item-5", "title": "chayal tatzdia", "title-heb": "חייל תצדיע",
+  "lines": []},
+  {"id": "grid-item-6", "title": "acharey she mitahavim", "title-heb": "אחרי שמתאהבים",
+  "lines": []},
+  {"id": "grid-item-7", "title": "savta lama ha pe shelach colkach gadol", "title-heb": "סבתא, למה הפה שלך גדול כל כך",
+  "lines": []},
+  {"id": "grid-item-8", "title": "titze bahutz", "title-heb": "תצא בחוץ",
+  "lines": []},
+  {"id": "grid-item-9", "title": "meyutar meyutar", "title-heb": "מיותר מיותר",
+  "lines": []},
+  {"id": "grid-item-10", "title": "havanta shel ha poza", "title-heb": "העוונטה של הפוזה",
+  "lines": []},
+  {"id": "grid-item-11", "title": "haorech lo kovea", "title-heb": "תארוז לי מהר",
+  "lines": []},
+  {"id": "grid-item-12", "title": "GPS", "title-heb": "תארוז לי מהר",
+  "lines": []},
+  {"id": "grid-item-13", "title": "sheket she yihye po", "title-heb": "תארוז לי מהר",
+  "lines": []},
+  {"id": "grid-item-14", "title": "shmor et hayadayim shelcha leatzmecha", "lines": []},
+  {"id": "grid-item-15", "title": "tziporey layla", "lines": []},
+  {"id": "grid-item-16", "title": "mehaa harifa", "lines": []},
+  {"id": "grid-item-17", "title": "ani be helem ani hamuma", "lines": []},
+  {"id": "grid-item-18", "title": "ego rtrip", "lines": []},
+  {"id": "grid-item-19", "title": "festival kan", "lines": []},
+  {"id": "grid-item-20", "title": "sa le shalom hamavtechot bifnim", "lines": []},
+  {"id": "grid-item-21", "title": "nire otchem be od kama shanim", "lines": []},
+  {"id": "grid-item-22", "title": "kvish 1", "lines": []},
+  {"id": "grid-item-23", "title": "al tidhof et ha af shelcha", "lines": []},
+  {"id": "grid-item-24", "title": "eize chom", "lines": []},
   "1","2","3"]
 }
